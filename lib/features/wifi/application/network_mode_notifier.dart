be036ad70252013bed274bf5b0e660eb359627cd
Jakob Schlögl <jakob.schloegl@htlstp.at>
2025-10-20 08:14:38 +0200

    feat: Enhance NetworkModeNotifier with logging and fix state handling in wifi_home_screen
    Signed-off-by: Jakob Schlögl <jakob.schloegl@htlstp.at>

--- lib/features/wifi/application/network_mode_notifier.dart
+++ lib/features/wifi/application/network_mode_notifier.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stpvelox/core/logging/has_logging.dart';
 import 'package:stpvelox/features/settings/domain/usecases/set_network_mode.dart';
 import 'package:stpvelox/features/wifi/application/wifi_provider.dart';
 import 'package:stpvelox/features/wifi/domain/application/network_mode_state.dart';
 import 'package:stpvelox/features/wifi/domain/enities/network_mode.dart';
 import 'package:stpvelox/features/wifi/usecases/get_network_mode.dart';
 
-class NetworkModeNotifier extends Notifier<NetworkModeState> {
+class NetworkModeNotifier extends Notifier<NetworkModeState> with HasLogger {
   late final GetNetworkMode _getNetworkMode;
   late final SetNetworkMode _setNetworkMode;
 
@@ -31,8 +32,10 @@ class NetworkModeNotifier extends Notifier<NetworkModeState> {
     try {
       await _setNetworkMode(mode);
       state = state.copyWith(mode: mode, isLoading: false);
+      log.info("Network mode updated to ${state.mode}");
     } catch (e) {
       state = state.copyWith(errorMessage: e.toString(), isLoading: false);
+      log.severe("Failed to update network mode: $e");
     }
   }
 }

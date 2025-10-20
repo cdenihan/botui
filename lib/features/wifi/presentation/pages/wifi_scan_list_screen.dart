bc5b6751b0b3d0058041e4f9882c8bc763f48446
Jakob Schlögl <jakob.schloegl@htlstp.at>
2025-10-20 09:38:24 +0200

    feat: Remove unused import for wifi_client_state in wifi_scan_list_screen
    Signed-off-by: Jakob Schlögl <jakob.schloegl@htlstp.at>

--- lib/features/wifi/presentation/pages/wifi_scan_list_screen.dart
+++ lib/features/wifi/presentation/pages/wifi_scan_list_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stpvelox/core/widgets/top_bar.dart';
 import 'package:stpvelox/features/wifi/application/wifi_client_notifier.dart';
-import 'package:stpvelox/features/wifi/domain/application/wifi_client_state.dart';
 import 'package:stpvelox/features/wifi/presentation/pages/wifi_manual_connect_screen.dart';
 import 'package:stpvelox/features/wifi/presentation/widgets/wifi_scan_list_item.dart';
 

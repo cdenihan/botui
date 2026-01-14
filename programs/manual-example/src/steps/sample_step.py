df827615fb058f07073636fa24bc7a9f506cc61e
js <jakob.schloegl@htlstp.at>
2026-01-14 08:25:31 +0100

    implement Fallback logic and fix inconsistent runscript defaults
    Signed-off-by: js <jakob.schloegl@htlstp.at>

--- programs/manual-example/src/steps/sample_step.py
+++ programs/manual-example/src/steps/sample_step.py
@@ -1,8 +1,11 @@
 from libstp.motor import Motor
+from libstp.step import Step
+
 
 class SampleStep(Step):
     pass
 
-@dsl
+
+# TODO: @dsl decorator is not defined - implement or import from appropriate module
 def sample_step(my_arg: bool, other_arg: str, sensor: Motor) -> SampleStep:
     return SampleStep()

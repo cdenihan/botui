4cf729b7a996611386d9c9e3557ebf1a0d5b64a3
tobias Madlberger <tobias.madlberger@gmail.com>
2026-01-27 07:50:17 +0100

    deploy flutter-ui-service

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,23 @@ for arg in "$@"; do
   esac
 done
 
+# Build Flutter app
 flutterpi_tool build --arch=arm64 --cpu=pi3 --release
 
+# Copy Flutter build
 rsync -a --info=progress2 ./build/flutter-pi/pi3-64/ \
   $USER@$HOST:/home/$USER/stp-velox
+
+# Copy systemd service file
+rsync -a ./systemd/flutter-ui.service \
+  $USER@$HOST:/home/$USER/flutter-ui.service
+
+# Move service file + reload systemd
+ssh $USER@$HOST "
+  sudo mv /home/$USER/flutter-ui.service /etc/systemd/system/flutter-ui.service &&
+  sudo systemctl daemon-reload
+  sudo systemctl enable flutter-ui.service
+"
 
 if [ "$NO_RESTART" = false ]; then
   ssh $USER@$HOST "sudo systemctl restart flutter-ui"
